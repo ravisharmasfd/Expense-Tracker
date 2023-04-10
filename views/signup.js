95752ca65e32f17738e319af42250d1453cc8bb0
RAVI SHARMA <118259227+ravisharmasfd@users.noreply.github.com>
2023-04-10 11:48:57 +0530

    Signup Page

---
+++ views/signup.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const submitBtn = document.getElementById('submit-btn');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	// TODO: add form validation and submission code
+});
+2
